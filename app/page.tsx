515f11a443908e67cc7a94f41fbb85c431b4d86e
Reynir Gunnarsson <reynirg20@ru.is>
2023-05-14 23:20:27 +0000

    add prisma and stuff, got data from db

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
+import { PrismaClient } from "@prisma/client";
 
-export default function Home() {
+const prisma = new PrismaClient();
+
+export default async function Home() {
+  const exercises = await prisma.exercise.findMany();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold underline">Hello Wlog</h1>
+      <h1 className="text-3xl font-bold underline">Wlogger</h1>
+      <ul>
+        {exercises.map((e) => {
+          return (
+            <li key={e.exercise_id}>
+              {e.name} - {e.description}
+            </li>
+          );
+        })}
+      </ul>
     </main>
   );
 }
