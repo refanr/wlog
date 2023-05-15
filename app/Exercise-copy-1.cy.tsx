933d9db9fa728505ded9aa800a5af5ab4c4f14a6
Reynir Gunnarsson <reynirg20@ru.is>
2023-05-15 11:59:03 +0000

    Revert "add cypress"
    This reverts commit 6228928fbcbf99b3e4757ad2a395df0eefb6a83f.

--- app/Exercise-copy-1.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Exercise from './Exercise'
-
-describe('<Exercise />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Exercise />)
-  })
-})
--- app/[exercise]/pageLogExercise.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import LogExercise from './page'
-
-describe('<LogExercise />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<LogExercise />)
-  })
-})
